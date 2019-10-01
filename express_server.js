34a8e470d81e2397af3be62856ada7bdc3422a6d
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-01 00:45:45 +0000

    URL Shortening Part 1

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+
+function generateRandomString(length) { // solution found https://itsolutionstuff.com/post/how-to-generate-random-string-in-javascriptexample.html 
+  let text = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let x of possible) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  }
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,6 +33,10 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -29,6 +45,11 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok");
 });
 
 app.listen(PORT, () => {
